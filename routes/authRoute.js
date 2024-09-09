e827b74e0415d3d52cde8b9bcf5a166edfd2f3e3
Appmontize <balram@appmontize.co.in>
2024-09-09 16:28:56 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,4 +5,22 @@ const router = express.Router();
 router.post('/signup', register);
 router.post('/login', login);
 
+
+router.get('/users', async (req, res) => {
+    try {
+      const users = await User.findAll({
+        attributes: ['user_id', 'name', 'email', 'created_at'],
+        include: [{
+          model: Wallet,
+          attributes: ['coins'] // Include coins field from Wallet model
+        }]
+      });
+      res.status(200).json({ users });
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      res.status(500).json({ error: 'An error occurred while fetching users.' });
+    }
+  });
+
+
 module.exports = router;
